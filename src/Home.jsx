364639b1a59af0f2562624c788f06c6c764464af
Arun Mathiyalakan <friday99@outlook.it>
2024-06-24 14:02:40 +0200

    ADD: bg to Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,16 @@
+import { Box } from '@chakra-ui/react'
+
 import NavBar from './components/NavBar.jsx'
 import Polaroids from './components/Polaroids.jsx'
 
+import background from './images/background.png'
+
 const Home = () => {
   return (
-    <>
+    <Box bgImg={background} bgPosition='center' bgRepeat='no-repeat' bgSize='cover'>
       <NavBar />
       <Polaroids />
-    </>
+    </Box>
   )
 }
 
