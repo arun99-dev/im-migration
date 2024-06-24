be237e4427f7511241ba3779ed49e74114895df9
Arun Mathiyalakan <friday99@outlook.it>
2024-06-24 12:34:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <ChakraProvider>
+      <h1>I&apos;M-Migration</h1>
+    </ChakraProvider>
+  )
+}
+
+export default App
