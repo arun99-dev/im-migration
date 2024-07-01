aaa26001779b6e590da22ba7da2f2675fde84007
Arun Mathiyalakan <friday99@outlook.it>
2024-07-01 18:43:37 +0200

    ADD: Scroll progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,15 @@ import Klevis from './components/Klevis.jsx'
 import Navbar from './components/NavBar.jsx'
 import Scroll from './components/Scroll.jsx'
 import AboutUs from './components/AboutUs.jsx'
+import { motion, useScroll } from "framer-motion";
 
 import './App.css'
 
 const App = () => {
   const [route, setRoute] = useState('')
   const [selectedLang, setSelectedLang] = useState(0)
+  const { scrollYProgress } = useScroll()
+
   return (
     <>
       {route && route !== 'aboutUs' && <Scroll />}
@@ -26,6 +29,22 @@ const App = () => {
       {route === 'elena' && <Elena selectedLang={selectedLang} />}
       {route === 'jian' && <Jian selectedLang={selectedLang} />}
       {route === 'aboutUs' && <AboutUs selectedLang={selectedLang} />}
+      {route && (
+        <motion.div
+          className="progress-bar"
+          style={{
+            scaleX: scrollYProgress,
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            zIndex: 1,
+            height: '5px',
+            background: 'var(--red)',
+            transformOrigin: '0%',
+          }}
+        />
+      )}
     </>
   )
 }
